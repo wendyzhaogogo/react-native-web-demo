bf36efb8ebc9a89713bc78e959e1416a6bd37ee7
wendy <zhaowenzzl@163.com>
2025-05-31 17:30:30 +0800

    Initial commit
    Generated by create-expo 3.4.2.

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import "./global.css";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
