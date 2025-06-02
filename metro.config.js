0ef4c40148e27785c4a43330f0e171398164c202
wendy <zhaowenzzl@163.com>
2025-06-02 17:25:14 +0800

    feat: release to githubpage

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname, {
+  // 配置 web 平台
+  platforms: ['web'],
+  // 添加 web 配置
+  web: {
+  }
+});
 
 module.exports = withNativeWind(config, { input: './global.css' }) 
